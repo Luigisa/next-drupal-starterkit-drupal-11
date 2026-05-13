ef0bd4c880b12d7a1ab0eb15dc7ab4ec9a8259b1
Luis <luis.gil@jakala.com>
2026-05-13 14:33:25 +0200

    Add theme

---
+++ drupal/web/themes/custom/wunder_claro/js/wunder-claro.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * JS opcional del subtema Wunder Claro.
+ */
+(function (Drupal) {
+  'use strict';
+
+  Drupal.behaviors.wunderClaro = {
+    attach(context) {
+      // Punto de enganche para comportamientos del admin.
+    },
+  };
+})(Drupal);
